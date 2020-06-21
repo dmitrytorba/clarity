8632976bb2ee1df653752a2717789ff53059ae97
Dmitry Torba <dmitrytorba@gmail.com>
2020-06-21 11:12:50 -0700

    logging

--- clarity-language-server/main.js
+++ clarity-language-server/main.js
@@ -1,9 +1,25 @@
-
+const fs = require('fs')
 const {spawn} = require('child_process');
+const pino = require('pino')
+const { multistream } = require('pino-multi-stream')
 
-if (process.platform === "linux") {
-    binaryName = "clarity-lsp-linux"
+// cant use stdio, so make our own
+var log = fs.createWriteStream("/tmp/clarity.log.txt", {flags:'a'});
+var streams = [
+  {level: 'debug', stream: log},
+  {level: 'error', stream: log},
+  {level: 'fatal', stream: log}
+]
+
+const logger = pino({
+  name: 'clarity-lsp',
+  level: 'info',
+}, multistream(streams))
+
+if (process.platform === 'linux') {
+    binaryName = '/tmp/clarity-lsp-linux'
 }
-
+logger.info('starting server')
 const childProcess = spawn(binaryName, [],
     {stdio: [process.stdin, process.stdout, process.stderr]});
+logger.info('exiting')
--- plugin.py
+++ plugin.py
@@ -3,19 +3,16 @@ import webbrowser
 
 from lsp_utils import NpmClientHandler
 
-
 def plugin_loaded():
     LspEslintPlugin.setup()
 
-
 def plugin_unloaded():
     LspEslintPlugin.cleanup()
-
 
 class LspEslintPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = 'clarity-language-server'
     server_binary_path = os.path.join(
-        server_directory, 'bin', 'main.js'
+        server_directory, 'main.js'
     )
 
