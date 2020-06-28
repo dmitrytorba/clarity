6bb81bf55a20188f6542caee9929478d0b2083a7
Dmitry Torba <dmitrytorba@gmail.com>
2020-06-28 08:56:12 -0700

    binary downloader

--- clarity-language-server/main.js
+++ clarity-language-server/main.js
@@ -1,7 +1,10 @@
-const fs = require('fs')
+const fs = require('fs');
+const http = require('http');
 const {spawn} = require('child_process');
-const pino = require('pino')
-const { multistream } = require('pino-multi-stream')
+const pino = require('pino');
+const { multistream } = require('pino-multi-stream');
+const request = require('request')
+
 
 // cant use stdio, so make our own
 var log = fs.createWriteStream("/tmp/clarity.log.txt", {flags:'a'});
@@ -16,10 +19,32 @@ const logger = pino({
   level: 'info',
 }, multistream(streams))
 
+
+var binaryUrl = 'https://github.com/lgalabru/clarity-lsp/releases/download/2020-05-27/'
 if (process.platform === 'linux') {
-    binaryName = '/tmp/clarity-lsp-linux'
+    binaryName = 'clarity-lsp-linux';
 }
-logger.info('starting server')
-const childProcess = spawn(binaryName, [],
-    {stdio: [process.stdin, process.stdout, process.stderr]});
-logger.info('exiting')
+binaryUrl += binaryName;
+
+const download = (url, path, callback) => {
+  request.head(url, (err, res, body) => {
+    request(url)
+      .pipe(fs.createWriteStream(path))
+      .on('close', callback)
+  })
+}
+
+const startServer = (err) => {
+    if (err) {
+        logger.error(err)
+    } else {
+        const fd = fs.openSync(binaryName, "r");
+        fs.fchmodSync(fd, 0o777);
+        logger.info('starting server');
+        const childProcess = spawn('./' + binaryName, [],
+            {stdio: [process.stdin, process.stdout, process.stderr]});
+        logger.info('exiting');
+    }
+}
+
+download(binaryUrl, binaryName, startServer);
